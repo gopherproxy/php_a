6d8db0a0454003ee6c4544eacfdf15aa9fadadee
gopherproxy <gopherproxy@gmail.com>
2017-03-19 13:30:40 +0100

    PHP start

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP start</title>
+</head>
+
+<body>
+<?php echo "Welcome to PHP - Your first steps into a bigger world!"?>
+</body>
+</html>
