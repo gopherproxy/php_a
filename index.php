8f4b0b793411973ccf96e449665e72cfb79f603b
gopherproxy <gopherproxy@gmail.com>
2017-03-20 15:15:58 +0100

    index.php with hello world

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hello world - in PHP!</title>
+</head>
+
+<body>
+<?php 
+// php one-line kommentar: her åbner en php code block 
+echo "Hello world!";
+// her slutter php:
+?>
+</body>
+</html>
